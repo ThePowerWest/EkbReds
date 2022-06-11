b9016046571f04bb6e01a071cc61b0cb36aa2ddc
Pavel Kotlyachkov <thepowerwest@gmail.com>
2022-06-11 20:21:31 +0500

    Изменил страницы пользователей и ролей Удалил ненужную колонку из таблицы пользователей в БД

---
+++ Infrastructure/Migrations/20220611141242_Delete_IsConfirmed.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    public partial class Delete_IsConfirmed : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsConfirmed",
+                table: "AspNetUsers");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsConfirmed",
+                table: "AspNetUsers",
+                type: "bit",
+                nullable: false,
+                defaultValue: false);
+        }
+    }
+}
